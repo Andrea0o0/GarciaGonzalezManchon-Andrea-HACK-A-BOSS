13c5f2c536f4cbf731a1c447f23506ca199bfaad
Andrea0o0 <andreagarciagonzalez22@gmail.com>
2023-12-14 02:56:36 +0100

    All first practical exercises

---
+++ WEEK 1/First Practical Exercises/Exercise1/src/main/java/practicalexercises/Main.java
@@ -0,0 +1,15 @@
+package practicalexercises;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name;
+        System.out.println("Please enter your name: ");
+        name = sc.next();
+        System.out.println("Hello " + name + ", welcome to the system");
+    }
+
+}
